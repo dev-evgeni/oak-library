762fdf6c214872298a7c00ef47eaa64195ca7e0b
dev-evgeni <dev.evgeni@gmail.com>
2022-12-21 09:17:39 +0200

    Added Material Design stylesheet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from PySide2.QtWidgets import QApplication, QMainWindow
 from main_window import Ui_MainWindow
+from qt_material import apply_stylesheet
 
 
 class MainWindow(QMainWindow):
@@ -14,6 +15,9 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     window = MainWindow()
+
+    apply_stylesheet(app, theme='dark_teal.xml')
+
     window.show()
 
     sys.exit(app.exec_())
